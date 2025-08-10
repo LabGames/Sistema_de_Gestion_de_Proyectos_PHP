0f68d511679dd754fb2f935a693b92968b188ce7
LabGames <99832300+LabGames@users.noreply.github.com>
2025-08-10 17:47:33 -0500

    update commit

--- php/gestors/proyects_gestor.php
+++ php/gestors/proyects_gestor.php
@@ -6,6 +6,40 @@
     <title>Gestor de Proyectos</title>
 </head>
 <body>
-    
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <a name="tittle">EMPRESA X</a>
+                    <button name="tittle_button">Regresar</button>
+                    <button name="tittle_button">Siguiente -></button>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>PROYECTOS REGISTRADOS</h1>
+        <nav>
+            <ul>
+                <li>
+                    <h3>Tipo de Proyectos:</h3>
+                    <a>Importante (20%) /</a>
+                    <a>Semi Importantes (10%) /</a>
+                    <a>Secundarios (05%).</a>
+                    <h3>Tiempo por Proyecto:</h3>
+                    <a>Proyecto 1: 7 meses / </a>
+                    <a>Proyecto 2: 2 meses / </a>
+                    <a>Proyecto 3: 1 año.</a>
+                    <h3>Productividad de los Colaboradores</h3>
+                    <a>Proyecto 1: Decente /</a>
+                    <a>Proyecto 2: Excelente /</a>
+                    <a>Proyecto 3: Mala.</a>
+                </li>
+            </ul>
+        </nav>
+    </main>
+    <footer>
+        <p>Logo de la empresa aqui.</p>
+    </footer>
 </body>
 </html>
--- php/modules/report_module.php
+++ php/modules/report_module.php
@@ -6,6 +6,53 @@
     <title>Modulo de Reportes</title>
 </head>
 <body>
-    
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <a name="tittle">EMPRESA X</a>
+                    <button name="tittle_button">Regresar</button>
+                    <button name="tittle_button">Siguiente -></button>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>GRAFICO CIRCULAR AQUI</h1>
+        <nav>
+            <ul>
+                <li>
+                    <h3>Tipo de Proyectos:</h3>
+                    <a>Importante (20%) /</a>
+                    <a>Semi Importantes (10%) /</a>
+                    <a>Secundarios (05%).</a>
+                    <h3>Tiempo por Proyecto:</h3>
+                    <a>Proyecto 1: 7 meses / </a>
+                    <a>Proyecto 2: 2 meses / </a>
+                    <a>Proyecto 3: 1 año.</a>
+                    <h3>Productividad de los Colaboradores</h3>
+                    <a>Proyecto 1: Decente /</a>
+                    <a>Proyecto 2: Excelente /</a>
+                    <a>Proyecto 3: Mala.</a>
+                </li>
+            </ul>
+        </nav>
+        <h1>GRAFICO DE BARRAS AQUI</h1>
+        <nav>
+            <ul>
+                <li>
+                    <h2>Tipo de Proyectos:</h2>
+                    <a>Importante</a>
+                    <h2>Tiempo por Proyecto:</h2>
+                    <a>7 meses</a>
+                    <h2>Productividad de los Colaboradores</h2>
+                    <a>Decente</a>
+                </li>
+            </ul>
+        </nav>
+    </main>
+    <footer>
+        <p>Logo de la empresa aqui.</p>
+    </footer>
 </body>
 </html>
