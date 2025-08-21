3a60815a01b9fb847ca63edb0376b67e6389ef4e
LabGames <99832300+LabGames@users.noreply.github.com>
2025-08-21 12:07:10 -0500

    update commit

--- app/Views/admin_manager.php
+++ app/Views/admin_manager.php
@@ -21,24 +21,24 @@
                     <a class="mini-subtitle">Proyecto Asociado: Campaña para el medio ambiente:</a>
                 </li>
                 <li class="user">
-                    <input class="name" type="text" name="project_name" value="Miembro 1" required readonly>
-                    <img class="icon" src="../images/logo.png.webp" alt="Planeta" class="task-image" width="50" height="50">
+                    <input class="name" type="text" name="member" value="Miembro 1" required readonly>
+                    <img class="icon" src="<?= BASE_URL ?>/Assets/images/logo.png.webp" alt="Planeta" class="task-image" width="50" height="50">
+                    <button type="submit" class="btn-style" name="delete">Eliminar</button>
+                </li>
+                <li class="user">
+                    <input class="name" type="text" name="member" value="Miembro 2" required readonly>
+                    <img class="icon" src="<?= BASE_URL ?>/Assets/images/logo.png.webp" alt="Planeta" class="task-image" width="50" height="50">
                     <button type="submit" class="btn-style">Eliminar</button>
                 </li>
                 <li class="user">
-                    <input class="name" type="text" name="project_name" value="Miembro 2" required readonly>
-                    <img class="icon" src="../images/logo.png.webp" alt="Planeta" class="task-image" width="50" height="50">
-                    <button type="submit" class="btn-style">Eliminar</button>
+                    <input class="name" type="text" name="member" value="Miembro 3" required readonly>
+                    <img class="icon" src="<?= BASE_URL ?>/Assets/images/logo.png.webp" alt="Planeta" class="task-image" width="50" height="50">
+                    <button type="submit" class="btn-style" name="delete" name="delete">Eliminar</button>
                 </li>
                 <li class="user">
-                    <input class="name" type="text" name="project_name" value="Miembro 2" required readonly>
-                    <img class="icon" src="../images/logo.png.webp" alt="Planeta" class="task-image" width="50" height="50">
-                    <button type="submit" class="btn-style">Eliminar</button>
-                </li>
-                <li class="user">
-                    <input class="name" type="text" name="project_name" value="Miembro 2" required readonly>
-                    <img class="icon" src="../images/logo.png.webp" alt="Planeta" class="task-image" width="50" height="50">
-                    <button type="submit" class="btn-style">Eliminar</button>
+                    <input class="name" type="text" name="member" value="Miembro 4" required readonly>
+                    <img class="icon" src="<?= BASE_URL ?>/Assets/images/logo.png.webp" alt="Planeta" class="task-image" width="50" height="50">
+                    <button type="submit" class="btn-style" name="delete">Eliminar</button>
                 </li>
                 <li>
                     <button type="submit" class="btn-style">Añadir Jefe de Proyecto</button>
@@ -49,7 +49,7 @@
 
     <footer> 
         <button class="title_button">Regresar</button>
-        <img src="../images/logo.png.webp" alt="Planeta" class="task-image" width="200" height="200">
+        <img src="<?= BASE_URL ?>/Assets/images/logo.png" alt="Planeta" class="task-image" width="200" height="200">
     </footer>
 </body>
 </html>
