a02e7609048d049a6edd1b221177de13e935604f
DiegxRG <diegoramosg3b@gmail.com>
2025-09-11 10:26:45 -0500

    Update Tarea.php

--- app/Models/tareas/Tarea.php
+++ app/Models/tareas/Tarea.php
@@ -6,6 +6,16 @@ class Tarea {
         $this->pdo = $pdo;
     }
 
+    public function getAll()
+    {
+        $sql = "SELECT t.*, e.nombre AS estado_nombre
+            FROM tareas t
+            INNER JOIN estados_tareas e ON t.estado_id = e.id";
+
+        $stmt = $this->pdo->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
     public function crear($titulo, $descripcion, $fecha_limite, $prioridad) {
         $sql = "INSERT INTO tareas (titulo, descripcion, fecha_limite, prioridad, estado_id, created_at, updated_at)
                 VALUES (:titulo, :descripcion, :fecha_limite, :prioridad, 1, NOW(), NOW())";
